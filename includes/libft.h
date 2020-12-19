cc880f858da7e00d5fae727eb5063c6fa1ce231b
mstoneho <mstoneho@student.21-school.ru>
2020-12-19 14:46:28 +0300

    clean sheet

--- includes/libft.h
+++
@@ -1,9 +0,0 @@
-#ifndef FT_PRINTF_LIBFT_H
-# define FT_PRINTF_LIBFT_H
-# include <stdlib.h>
-
-char		*ft_strchr(const char *s, int c);
-size_t		ft_strlen(const char *str);
-int			ft_atoi(char *str);
-
-#endif
